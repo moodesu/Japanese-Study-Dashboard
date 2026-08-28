9d051effc62c2137bc4930820a19cb58b26baf43
moodesu <130012930+moodesu@users.noreply.github.com>
2026-08-28 20:30:19 +0900

    input wanikani token

--- wanikani-config.js
+++ wanikani-config.js
@@ -2,5 +2,5 @@
 // Add your personal v2 API token below before deploying.
 // Keep this file out of a public repository if it contains your real token.
 window.WANIKANI_CONFIG = {
-  apiToken: 'YOUR_WANIKANI_API_TOKEN'
+  apiToken: '894c4a72-b082-47d6-8d79-6543f2c8206c'
 };
